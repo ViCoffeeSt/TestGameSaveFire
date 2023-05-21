4f812fca200a705029a1fbe268984d826aabcd55
ViCoffeeSt <drtabaluga780@gmail.com>
2023-05-21 22:28:34 +0300

    Update

--- Assets/Scripts/TorchController.cs
+++ Assets/Scripts/TorchController.cs
@@ -2,12 +2,14 @@ using UnityEngine;
 
 public class TorchController : MonoBehaviour
 {
-    public Transform playerHand;
-    public Transform torchModel;
+    public Transform playerHand; // —сылка на кость или точку прив€зки руки игрока
+    public Transform torchModel; // —сылка на модель факела
+    public float x, y, z;
 
     void Update()
     {
+        // ”становите позицию и поворот факела равными позиции и повороту руки игрока
         torchModel.position = playerHand.position;
-        torchModel.rotation = playerHand.rotation;
+        torchModel.rotation = playerHand.rotation * Quaternion.Euler(x,y,z);
     }
 }
